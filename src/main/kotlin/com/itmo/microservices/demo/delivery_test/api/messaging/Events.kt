72a834c48e28d121a1880050a6583b47f37dd1c7
Felix Ardi <sonik-gx@mail.ru>
2022-11-21 21:23:08 +0300

    delete unused files

--- src/main/kotlin/com/itmo/microservices/demo/delivery_test/api/messaging/Events.kt
+++
@@ -1,7 +0,0 @@
-package com.itmo.microservices.demo.users.api.messaging
-
-import com.itmo.microservices.demo.users.api.model.UserDTO
-
-data class UserCreatedEvent(val user: UserDTO)
-
-data class UserDeletedEvent(val username: String)
--- src/main/kotlin/com/itmo/microservices/demo/delivery_test/api/model/RegistrationRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.itmo.microservices.demo.users.api.model
-
-data class RegistrationRequest(
-        val name: String,
-        val password: String
-)
