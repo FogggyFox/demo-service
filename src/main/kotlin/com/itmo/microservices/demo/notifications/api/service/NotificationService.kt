4c638e8ac345c7fb57b476133dae713829b51f0b
вфк <284350@niuitmo.ru>
2023-01-05 14:51:32 +0500

    clean up

--- src/main/kotlin/com/itmo/microservices/demo/notifications/api/service/NotificationService.kt
+++
@@ -1,9 +0,0 @@
-package com.itmo.microservices.demo.notifications.api.service
-
-import com.itmo.microservices.demo.tasks.api.model.TaskModel
-import com.itmo.microservices.demo.users.api.model.UserDTO
-
-interface NotificationService {
-    fun processNewUser(user: UserDTO)
-    fun processAssignedTask(task: TaskModel)
-}
